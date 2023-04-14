ddceae59af4bbfa8111a7e5c4fa76089cf584916
demonkillerr <gaurangvishwakarma619@gmail.com>
2023-04-14 06:18:23 +0530

    [UPDATE] bootable USB script

--- bootableusb.sh
+++ bootableusb.sh
@@ -3,4 +3,4 @@ umount /dev/sdb*
 
 sudo mkfs.ext4 /dev/sdb
 
-sudo dd if=out/tcet-* of=/dev/sdb status=progress bs-2048
+sudo dd if=out/tcetlinux-$(date +%Y.%m.%d).iso of=/dev/sdb status=progress bs-2048
