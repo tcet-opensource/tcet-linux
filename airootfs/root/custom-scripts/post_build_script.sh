a121cb5a903b8a51803c4396301447efa423f0ca
harshau007 <amanupadhyay2004@gmail.com>
2023-08-08 23:43:06 +0530

    [DELETED] custom_scripts

--- airootfs/root/custom-scripts/post_build_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Path to calamares.desktop in the ISO's root
-CALAMARES_DESKTOP="/etc/skel/Desktop/calamares.desktop"
-
-# Make calamares.desktop executable
-chmod +x "$CALAMARES_DESKTOP"
