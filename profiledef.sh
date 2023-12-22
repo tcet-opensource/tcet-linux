cb65c04919daf30da3c77725e6fef45124b6ffee
Akash6222 <akashdubey25274@gmail.com>
2023-12-23 01:56:33 +0530

    updated profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -17,7 +17,6 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/etc/skel/set_once_xfce4.sh"]="0:0:755"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
